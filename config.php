0c7df476b5ea714bc3ca17b0be9b6461db99248c
Threath <threathgfx@gmail.com>
2019-06-09 17:53:52 +0800

    User's configuration

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+    // CPANEL Server
+    $data['server']= "";
+    
+    // Default; 2083, Non SSL; 2082
+    $data['port']= "2083";
+    
+    // CPANEL username
+    $data['username']= "";
+    
+    // CPANEL password
+    $data['password']= "";
+    
+    // Chosen domain for mail (by default, your domain)
+    $data['email_server'] = "";
+    
+    // 500 is equivalent to 500MB
+    $data['megabytequota']= "500";
+
+?>
