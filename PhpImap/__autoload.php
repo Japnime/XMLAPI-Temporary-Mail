6767ab8844c7408363dd03ce0fb6d66a8a3f27c9
Threath <threathgfx@gmail.com>
2019-06-16 20:22:54 +0800

    Autoload Configuration for PhpImap

---
+++ PhpImap/__autoload.php
@@ -0,0 +1,11 @@
+<?php
+
+    namespace PhpImap;
+    
+    spl_autoload_register(function ($class)
+    {
+    	if(strpos($class, __NAMESPACE__) === 0)
+    	{
+    		require_once(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php');
+    	}
+    });
