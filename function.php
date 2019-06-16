758b68b3d3f3e73420dc931b3ff94ec4c8a392e7
Threath <threathgfx@gmail.com>
2019-06-16 20:29:13 +0800

    Added some minor features

--- function.php
+++ function.php
@@ -29,6 +29,38 @@
         return $key;
     }
 
+// Base64 Key Encryption/Decryption
+    function _data($string, $type)
+    {
+        $key[1] = "PWaanA*()!#EGyKaaZ";
+        $key[2] = "PWAsrqWUN*()!#RETyAAga";
+        
+        if($type == "encrypt")
+        {
+            $result = base64_encode(openssl_encrypt($string, "AES-256-CBC", $key[1], 0, $key[2]));
+        } elseif($type == "decrypt")
+        {
+            $result = openssl_decrypt(base64_decode($string), "AES-256-CBC", $key[1], 0, $key[2]);
+        }
+        return $result;
+    }
+    
+// Username Randomizer
+    function _randomizeUser()
+    {
+        $variable['consonant'] = "bcdfghjklmnprstvwz";
+        $variable['vowel'] = "aeiou";
+        $variable['combination'] = $variable['consonant'].$variable['vowel'];
+        
+        for ($i=0; $i < 2; $i++)
+        {
+            $result .= $variable['consonant'][rand(0, strlen($variable['consonant'])-1)];
+            $result .= $variable['vowel'][rand(0, strlen($variable['vowel'])-1)];
+            $result .= $variable['combination'][rand(0, strlen($variable['combination'])-1)];
+        }
+        return $result;
+    }
+
 // Call API to add the email
     function api_add_query($server, $port, $username, $password, $mailserver, $mailuser, $mailpassword, $mailqouta)
     {
@@ -47,7 +79,7 @@
         
         if ($result -> data -> result == 1)
         {
-            log_file($mailuser, $mailserver);
+            log_file($mailuser, $mailserver, $mailpassword);
             return 1; // Success
         } else {
             return $result  -> data -> reason; // Failed
@@ -71,12 +103,12 @@
     }
     
 // Internal logging for time stamp deletion
-    function log_file($userlog, $mailserver)
+    function log_file($userlog, $mailserver, $passlog)
     {
-        $log_file = $userlog;
+        $log_file = _data($userlog, "encrypt");
         $log_path = "email_log/$log_file";
         $implement = fopen($log_path, w);
-        $information = time().'@@'.$userlog.'@'.$mailserver;
+        $information = time().','.$userlog.'@'.$mailserver.','.$passlog;
         fwrite($implement, $information);
         fclose($implement);
     }
